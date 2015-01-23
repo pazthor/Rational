2b77719063845187dc07c60c698ce9d8e5de78b9
juls <final.africae@gmail.com>
2015-01-22 18:53:13 -0600

    primer commit
    Agrego la clase Rational vacía y con una descripción

---
+++ Rational.scala
@@ -0,0 +1,7 @@
+/**
+ *A Rational number is a number that can be
+ *expressed as a ratio n/d, where n and d are
+ *integers, except that d cannot be zero. n is called 
+ *the numerator and d the denominator
+ */
+class Rational {}
